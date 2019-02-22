8287838f8541db75ef25a68def83170db56b7182
David <davidsima@windowslive.com>
2019-02-23 00:48:47 +0200

    Dark theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css';
+const dark = () => {
+    document.getElementsByTagName("body")[0].classList.add("black")
+}
+const light = () => {
+    document.getElementsByTagName("body")[0].classList.remove("black")
+}
 const Header = () => {
     return <header className={styles.header}>
         <div className={`wrapper ${styles.wrapperAddon}`}>
             <h1 className={styles.title}>UniCALc<sup>II</sup></h1>
             <div className={styles.themes}>
-                <div>Light</div>
-                <div>Dark</div>
+                <div onClick={light}>Light</div>
+                <div onClick={dark}>Dark</div>
             </div>
         </div>
     </header>;
