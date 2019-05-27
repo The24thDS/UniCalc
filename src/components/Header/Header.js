071ac726fb422922ed289f73278669cdf410d14f
The24thDS <davidsima@windowslive.com>
2019-05-27 11:16:10 +0300

    Removed themes implementation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import styles from './Header.module.css';
-const dark = () => {
-    document.getElementsByTagName("body")[0].classList.add("black")
-}
-const light = () => {
-    document.getElementsByTagName("body")[0].classList.remove("black")
-}
 const Header = () => {
     return <header className={styles.header}>
         <div className={`wrapper ${styles.wrapperAddon}`}>
             <h1 className={styles.title}>UniCALc<sup>II</sup></h1>
-            <div className={styles.themes}>
-                <div onClick={light}>Light</div>
-                <div onClick={dark}>Dark</div>
-            </div>
         </div>
     </header>;
 }
