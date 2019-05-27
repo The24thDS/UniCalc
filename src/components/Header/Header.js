3bd80cc385ec7013492d3bef26c14c89fdced73d
The24thDS <davidsima@windowslive.com>
2019-05-27 11:27:35 +0300

    Adding semester buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,10 @@ const Header = () => {
         <div className={`wrapper ${styles.wrapperAddon}`}>
             <h1 className={styles.title}>UniCALc<sup>II</sup></h1>
         </div>
+        <div className={styles.menu}>
+            <span className={styles.active}>First semester</span>
+            <span>Second Semester</span>
+        </div>
     </header>;
 }
 export default Header;
