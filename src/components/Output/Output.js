2fe15c3d022d46280b3c6273867171550a07c3a3
The24thDS <davidsima@windowslive.com>
2020-02-13 16:58:57 +0200

    Changed Output to receive subjects and grades and calculate the average

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -1,72 +1,28 @@
-import React from 'react';
-import {Radar} from 'react-chartjs-2';
-import styles from './Output.module.css';
+import React from "react";
+import styles from "./Output.module.css";
 
-const Output = ({chartData, average}) => {
-    if(average)
-    {
-        const pointColors = ['#ff3900','#33658a','#86bbd8','#2f4858','#f6ae2d','#01fdf6','#cbbaed','#e9df00']
-        const data = {
-            datasets: [
-                {
-                    data: Object.values(chartData),
-                    label: 'Nota',
-                    backgroundColor: '#03fcba99',
-                    borderColor: '#989898',
-                    pointBackgroundColor: pointColors,
-                    pointBorderColor: "#fff",
-                    pointBorderWidth: 1,
-                    pointHoverBackgroundColor: pointColors,
-                    pointHoverBorderColor: pointColors,
-                    pointRadius: 5,
-                    pointHoverRadius: 7
-                }
-            ],
-            labels: Object.keys(chartData)
-        }
-        const style = {
-            displayColors: true
-        }
-        const scale = {
-            ticks: {
-                suggestedMin: 0,
-                suggestedMax: 10,
-                stepSize: 1,
-                display: false
-            },
-            pointLabels: {
-                display: true,
-                fontColor: '#000',
-                fontSize: 12
-            }
-        }
-        const options = {
-            maintainAspectRatio: true,
-            responsive: true,
-            aspectRatio: 1,
-            layout: {
-                padding: 10,
-            },
-            scale: scale,
-            legend: { display: false },
-            style
-        }
-        return(
-            <div className={styles.output}>
-                <h1>{Math.round(average * 100) / 100}</h1>
-                <Radar 
-                data={data} 
-                options={options}
-                />
-            </div>
-        )
-    } else {
-        return(
-            <div className={styles.output}>
-                <h1>{"Media ta va apărea aici alături de o diagramă fancy"}</h1>
-            </div>
-        )
-    }
-}
+const Output = ({ subjects, grades }) => {
+  if (grades.length > 0 && grades.length === subjects.length) {
+    let creditsTimesGradesSum = 0;
+    let creditsSum = 0;
+    subjects.forEach((subject, index) => {
+      creditsTimesGradesSum += Number(subject.credits) * Number(grades[index]);
+      creditsSum += Number(subject.credits);
+    });
+
+    const average = creditsTimesGradesSum / creditsSum;
+    return (
+      <div className={`${styles.output} ${styles.average}`}>
+        <h1>{Math.floor(average * 100) / 100}</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.output}>
+        <h1>Media ta va apărea aici.</h1>
+      </div>
+    );
+  }
+};
 
 export default Output;
