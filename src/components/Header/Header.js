43d1872d7e482069b569d1708f5ac2878b6eae2a
The24thDS <davidsima@windowslive.com>
2020-02-16 16:03:54 +0200

    Removed old props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "./Header.module.css";
-const Header = ({ navigation, semester, setSemester }) => {
+const Header = () => {
   return (
     <header className={styles.header}>
       <div className={`wrapper ${styles.wrapperAddon}`}>
